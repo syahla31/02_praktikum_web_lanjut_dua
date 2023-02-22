b27a3a8aa2ccdd9d5560889b83b40e7f4b2d31bd
syahla31 <syahlasyafiqah31@gmail.com>
2023-02-22 10:08:07 +0700

    Jobsheet 2 - Pratikum 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        echo '2141720015 - Syahla Syafiqah Fayra';
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AboutController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'selamat']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/article/{id}', [ArticleController::class, 'article']);
+
